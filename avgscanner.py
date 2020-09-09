d0dc69b988b716bc74a99bffcfbe15028c7129f8
TheAlpha19 <nidhinnazar@gmail.com>
2020-09-09 04:37:40 -0400

    OPT 1 AND 2 Structure Complete.. Maybe.

--- avgscanner.py
+++ avgscanner.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 #avgscanner.py -i <IP> --opt <1, 2, 3>
 import argparse
+import subprocess
 
 def exit():
     print("\n\t\t[+] Quits [+]\n")
     quit()
 
+def portFind(IP):
+    print("\t\t[+] Running Port Find [+]\n")
+    out = subprocess.run(['masscan', '-e', 'tun0', '--rate=1000', '-p', '0-65535', IP], capture_output = True)
+    print("{}".format(out.stdout.decode()))
+    print("\t\t[+] Port Find Complete [+]\n")
+
+def Scan(IP):
+    print("\t\t[+] Running Scan [+]\n")
+    subprocess.run(["nmap -T4 -A -v {} -oN Scan.txt".format(IP)], shell = True)
+    print("\t\t[+] NMAP Scan Complete [+]\n")
+
 parser = argparse.ArgumentParser(description = 'Your Average Scanner', epilog = 'Use Responsibly!')
 parser.add_argument('IP', help = "Target IP", nargs = '+')
-parser.add_argument('--opt', help = "Options Mode [Default -> 3]: 1 -> Port find, 2 -> NMAP scan, 3 -> Port Find and Run Scan. [Only on TCP Ports]", metavar = 'OPTION', default = 3)
+parser.add_argument('--opt', help = "Options Mode [Default -> 2]: 1 -> Port find, 2 -> NMAP scan, 3 -> Port Find and Run Scan. [Only on TCP Ports]", metavar = 'OPTION', default = 2)
 args = parser.parse_args()
 
 IP = args.IP[0]
@@ -22,4 +34,9 @@ if (opt < 1) or (opt > 3):
     print("Option Should Be 1, 2, or 3 (see --help)")
     exit()
 else:
-    print("All good!")
+    if opt == 1:
+        portFind(IP)
+    elif opt == 2:
+        Scan(IP)
+
+print("\n\t\t[+] Scan Complete [+]")
